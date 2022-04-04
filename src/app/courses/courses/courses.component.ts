776b09060b1510a355e094e2fe15d37e4c3cd7f1
Rhael Henrique <rhaelhenriquerodrigues@gmail.com>
2022-04-04 20:18:51 -0300

    spinner loading

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -10,7 +10,7 @@ import { Observable } from 'rxjs/internal/Observable';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Observable<Course[]>;
+  courses$: Observable<Course[]>;
 
   displayedColumns = ['name', 'category'];
 
@@ -19,7 +19,7 @@ export class CoursesComponent implements OnInit {
   constructor(private coursesService: CoursesService) {
     //this.courses = [];
     //this.coursesService = new CoursesService();
-    this.courses = this.coursesService.list();
+    this.courses$ = this.coursesService.list();
   }
 
   ngOnInit(): void {
