4ef7e970d61da4e343602e323e1a0ac12c7a2a25
Rhael Henrique <rhaelhenriquerodrigues@gmail.com>
2022-04-04 20:02:04 -0300

    observable

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
 
 import { Course } from './../model/course';
 
@@ -8,15 +9,14 @@ import { Course } from './../model/course';
 })
 export class CoursesService {
 
+  private readonly API = '/assets/courses.json';
+
   constructor(private httpClient: HttpClient) { }
 
-  list(): Course[] {
-    return [
-      {
-        _id: '1',
-        name: 'Angular',
-        category: 'Desenvolvimento'
-      }
-    ];
+  list() {
+    return this.httpClient.get<Course[]>(this.API)
+    .pipe(
+      tap(courses => console.log(courses))
+    );
   }
 }
